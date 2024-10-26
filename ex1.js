728b853cfe997068578387eefa0c8115d25e722d
unknown <ryanchehab20@gmail.com>
2024-10-26 15:00:37 +0300

    changed the code layout

--- ex1.js
+++ ex1.js
@@ -1,22 +1,22 @@
 let input = prompt("Please enter a positive number: ")
 
 if (!isNaN(input) && input !== "" && input>0){
-    console.log("valid number: ",input)
+    
+    counter=1;
+    let width = input * 2;
+    
+        for (let i = 0; i<input;i++){
+            console.log('*'.repeat(counter).padStart(width+counter/2))
+            counter+=2
+        }
+        counter-=4
+        for (let i= 0; i<input;i++){
+            console.log('*'.repeat(counter).padStart(width+counter/2))
+            counter-=2
+        }
 }
 else{
     console.log("You didnt enter a valid number")
 }
 
-counter=1;
-let width = input * 2;
 
-
-for (let i = 0; i<input;i++){
-    console.log('*'.repeat(counter).padStart(width+counter/2))
-    counter+=2
-}
-counter-=4
-for (let i= 0; i<input;i++){
-    console.log('*'.repeat(counter).padStart(width+counter/2))
-    counter-=2
-}
